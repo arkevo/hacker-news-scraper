289ef6c36f4c64c56ae34ba3f7e2d758192f2193
Ivo Kolev <arkevo@Ivos-MacBook-Pro.local>
2020-03-14 22:14:04 -0400

    more bug fixing

--- controllers/api/articles.js
+++ controllers/api/articles.js
@@ -100,7 +100,7 @@ router.delete("/:id", function(req, res) {
 });
 
 router.get(
-  "/scrape",
+  "/newArticles",
   function(req, res, next) {
     request("https://news.ycombinator.com/", function(
       error,
